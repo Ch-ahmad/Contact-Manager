b29b6a4de534ccafdefdefa0461c7f0d573f016d
Ahmad Yamien <ayamien@hawk.iit.edu>
2024-02-19 03:09:07 -0600

    troubleshooting

--- mycontactdb/app.js
+++ mycontactdb/app.js
@@ -1,41 +1,76 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function readContacts() {
+  try {
+    return JSON.parse(fs.readFileSync(contactsPath, 'utf8'));
+  } catch (err) {
+    if (err.code === 'ENOENT' || err.message.includes('Unexpected end of JSON input')) {
+      console.log('Contacts file is empty or does not exist, initializing with an empty array.');
+      return [];
+    } else {
+      throw err;
+    }
+  }
+}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+function writeContacts(contacts) {
+  fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/contacts', (req, res) => res.render('contacts', { contacts: readContacts() }));
+
+app.get('/contacts/new', (req, res) => res.render('new'));
+
+app.get('/contacts/:id', (req, res) => {
+  const contact = readContacts().find(c => c.id === req.params.id);
+  if (!contact) res.status(404).send('Contact not found');
+  else res.render('contact', { contact });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/contacts', (req, res) => {
+  const contacts = readContacts();
+  const newContact = { id: uuidv4(), ...req.body, created: new Date().toISOString(), lastEdited: new Date().toISOString() };
+  contacts.push(newContact);
+  writeContacts(contacts);
+  res.redirect(`/contacts/${newContact.id}`);
 });
 
-module.exports = app;
+app.get('/contacts/:id/edit', (req, res) => {
+  const contact = readContacts().find(c => c.id === req.params.id);
+  res.render('editContact', { contact });
+});
+
+app.put('/contacts/:id', (req, res) => {
+  let contacts = readContacts();
+  contacts = contacts.map(c => c.id === req.params.id ? { ...c, ...req.body, lastEdited: new Date().toISOString() } : c);
+  writeContacts(contacts);
+  res.redirect(`/contacts/${req.params.id}`);
+});
+
+app.delete('/contacts/:id', (req, res) => {
+  writeContacts(readContacts().filter(c => c.id !== req.params.id));
+  res.redirect('/contacts');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Something broke! Error: ${err.message}`);
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
--- mycontactdb/routes/users.js
+++ mycontactdb/routes/users.js
@@ -1,52 +1,54 @@
-var express = require('express');
-var router = express.Router();
-const { loadContacts, saveContacts, Contact } = require('../dataAccess'); 
+module.exports = (dependencies) => {
+  const express = require('express');
+  const router = express.Router();
+  const { loadContacts, saveContacts, Contact } = dependencies;
 
-// GET route for listing all contacts
-router.get('/contacts', function(req, res, next) {
-  const contacts = loadContacts();
-  res.render('contacts', { contacts }); // Use the 'contacts.pug' view for listing contacts
-});
-
-// POST route for adding a new contact
-router.post('/add-contact', function(req, res) {
-  const { firstName, lastName, email, notes } = req.body;
-  const newContact = new Contact(firstName, lastName, email, notes);
-  const contacts = loadContacts();
-  contacts.push(newContact);
-  saveContacts(contacts);
-  res.redirect('/users/contacts'); // Redirect to the listing page
-});
-
-// GET route for displaying the edit contact form
-router.get('/edit-contact/:id', function(req, res) {
-  const contacts = loadContacts();
-  const contact = contacts.find(contact => contact.id === req.params.id);
-  if (!contact) {
-    return res.send('Contact not found.');
-  }
-  res.render('editContact', { contact }); // Use 'editContact.pug' for the edit form
-});
-
-// POST route- updating a contact
-router.post('/edit-contact/:id', function(req, res) {
-  let contacts = loadContacts();
-  contacts = contacts.map(contact => {
-    if (contact.id === req.params.id) {
-      return { ...contact, firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, notes: req.body.notes };
-    }
-    return contact;
+  // GET route for listing all contacts
+  router.get('/contacts', function(req, res) {
+      const contacts = loadContacts();
+      res.render('contacts', { contacts });
   });
-  saveContacts(contacts);
-  res.redirect('/users/contacts'); // Redirect back to contacts list
-});
 
-// POST route- deleting a contact
-router.get('/delete-contact/:id', function(req, res) {
-  const contacts = loadContacts();
-  const filteredContacts = contacts.filter(contact => contact.id !== req.params.id);
-  saveContacts(filteredContacts);
-  res.redirect('/users/contacts'); // Redirect back to the contacts
-});
+  // POST route for adding a new contact
+  router.post('/add-contact', function(req, res) {
+      const { firstName, lastName, email, notes } = req.body;
+      const newContact = new Contact(firstName, lastName, email, notes);
+      const contacts = loadContacts();
+      contacts.push(newContact);
+      saveContacts(contacts);
+      res.redirect('/users/contacts');
+  });
 
-module.exports = router;
+  // GET route for displaying the edit contact form
+  router.get('/edit-contact/:id', function(req, res) {
+      const contacts = loadContacts();
+      const contact = contacts.find(contact => contact.id === req.params.id);
+      if (!contact) {
+          return res.status(404).send('Contact not found.');
+      }
+      res.render('editContact', { contact });
+  });
+
+  // POST route for updating a contact
+  router.post('/edit-contact/:id', function(req, res) {
+      let contacts = loadContacts();
+      contacts = contacts.map(contact => {
+          if (contact.id === req.params.id) {
+              return { ...contact, ...req.body, lastEdited: new Date().toISOString() };
+          }
+          return contact;
+      });
+      saveContacts(contacts);
+      res.redirect('/users/contacts');
+  });
+
+  // POST route for deleting a contact
+  router.post('/delete-contact/:id', function(req, res) {
+      let contacts = loadContacts();
+      contacts = contacts.filter(contact => contact.id !== req.params.id);
+      saveContacts(contacts);
+      res.redirect('/users/contacts');
+  });
+
+  return router;
+};
