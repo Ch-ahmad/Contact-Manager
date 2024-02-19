6cb34cb3e2fe8ea3b28fd8d17c38b93acf8cbd35
Ahmad Yamien <ayamien@hawk.iit.edu>
2024-02-18 21:31:46 -0600

    added post & get function

--- mycontactdb/routes/users.js
+++ mycontactdb/routes/users.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const Contact = require('../Contact'); // path
+const { loadContacts, saveContacts } = require('../dataAccess'); // path
 
-
-
-/* GET users listing. */
+//GET
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// POST
+router.post('/add-contact', function(req, res) {
+  // contact detail
+  const { firstName, lastName, email, notes } = req.body;
+
+  
 });
 
 module.exports = router;
