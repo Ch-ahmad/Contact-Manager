c14ac3fbff90eac85aa9992ea31b0950733b3931
Ahmad Yamien <ayamien@hawk.iit.edu>
2024-02-18 22:56:35 -0600

    updated all the routes

--- mycontactdb/routes/users.js
+++ mycontactdb/routes/users.js
@@ -1,28 +1,52 @@
 var express = require('express');
 var router = express.Router();
-const Contact = require('../Contact'); // path
-const { loadContacts, saveContacts } = require('../dataAccess'); // path
+const { loadContacts, saveContacts, Contact } = require('../dataAccess'); 
 
-//GET
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET route for listing all contacts
+router.get('/contacts', function(req, res, next) {
+  const contacts = loadContacts();
+  res.render('contacts', { contacts }); // Use the 'contacts.pug' view for listing contacts
 });
 
-// POST
+// POST route for adding a new contact
 router.post('/add-contact', function(req, res) {
-  // contact detail
   const { firstName, lastName, email, notes } = req.body;
-
-  // Create contact
   const newContact = new Contact(firstName, lastName, email, notes);
-
-  // Load the current contacts, add new and save
   const contacts = loadContacts();
   contacts.push(newContact);
   saveContacts(contacts);
+  res.redirect('/users/contacts'); // Redirect to the listing page
+});
 
-  // Redirect 
-  res.redirect('/contacts');
+// GET route for displaying the edit contact form
+router.get('/edit-contact/:id', function(req, res) {
+  const contacts = loadContacts();
+  const contact = contacts.find(contact => contact.id === req.params.id);
+  if (!contact) {
+    return res.send('Contact not found.');
+  }
+  res.render('editContact', { contact }); // Use 'editContact.pug' for the edit form
+});
+
+// POST route- updating a contact
+router.post('/edit-contact/:id', function(req, res) {
+  let contacts = loadContacts();
+  contacts = contacts.map(contact => {
+    if (contact.id === req.params.id) {
+      return { ...contact, firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, notes: req.body.notes };
+    }
+    return contact;
+  });
+  saveContacts(contacts);
+  res.redirect('/users/contacts'); // Redirect back to contacts list
+});
+
+// POST route- deleting a contact
+router.get('/delete-contact/:id', function(req, res) {
+  const contacts = loadContacts();
+  const filteredContacts = contacts.filter(contact => contact.id !== req.params.id);
+  saveContacts(filteredContacts);
+  res.redirect('/users/contacts'); // Redirect back to the contacts
 });
 
 module.exports = router;
